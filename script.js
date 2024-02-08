1613c0eff7748ff791d94b588a5495dd93e4ba29
AOP <aop@flakio.com>
2024-02-08 11:02:09 -0500

    final version

--- script.js
+++ script.js
@@ -12,7 +12,13 @@ const generateDog = () => {
         return rand
     }
     // random function to be used in nested array
-
+    let newArr = []
+    for (let i = 0; i < Object.keys(categories).length; i++) {
+        catValue = Object.values(categories)[i]
+        randCatVal = randNum(catValue.length);
+        newArr.push(catValue[randCatVal])
+    }
+    return `You should ${newArr[1]} a ${newArr[0]} dog and name it ${newArr[3]}. I'm sure you will ${newArr[2]} it!`
 }
 
 console.log(generateDog())
