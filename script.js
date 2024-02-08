8c6d0358b17db9269764a671c25f5d6343f1148a
AOP <aop@flakio.com>
2024-02-08 10:46:39 -0500

    base script

---
+++ script.js
@@ -0,0 +1,18 @@
+const categories = {
+    breeds: ['Keeshond', 'Golden Retreiver', 'Husky', 'Poodle', 'Blood Hound'],
+    acquires: ['buy', 'adopt', 'rescue'],
+    emotions: ['love', 'adore', 'worship'],
+    names: ['Pogo', 'Kiwi', 'Leo', 'Potatoe']
+}
+// set the variants of adjectives and nouns
+
+const generateDog = () => {
+    const randNum = num => {
+        let rand = Math.floor(Math.random() * num)
+        return rand
+    }
+    // random function to be used in nested array
+
+}
+
+console.log(generateDog())
